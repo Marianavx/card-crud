22fc71e5d3e377cdea45546d8a23d52cf887a562
Your Name <you@example.com>
2023-05-01 20:09:31 -0500

    creacion de card

---
+++ src/assets/components/Page/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Crud} from '../Layouts/Crud/Crud'
+import {Card} from '../Layouts/Card/Card'
+
+export const Home = () => {
+  return (
+    <>
+         <Card/>
+    </>
+  )
+}
